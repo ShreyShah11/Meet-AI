db0e072ae752767d2e11f9e957aa4989c3cf94c5
ShreyShah11 <shrey111105@gmail.com>
2026-05-23 19:53:43 +0530

    Initial commit

--- worker/src/config/db.js
+++ worker/src/config/db.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/meetflow';
+    const conn = await mongoose.connect(mongoUri);
     console.log(`[Worker] MongoDB Connected: ${conn.connection.host}`);
 
     mongoose.connection.on('error', (err) => {
